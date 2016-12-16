b786c76a681d3373e646e03644bc2ba315db8f52
Feihong Hsu <feihong.hsu@gmail.com>
2016-12-16 13:30:58 -0600

    Add a comment about how arguments involving function or method invocations should be parenthesized

--- process/execute-process.fsx
+++ process/execute-process.fsx
@@ -7,4 +7,5 @@ info.RedirectStandardOutput <- true
 info.UseShellExecute <- false
 
 let proc = Process.Start(info)
+// Arguments involving function or method applications should be parenthesized.
 printfn "Result:\n%s" (proc.StandardOutput.ReadToEnd())
